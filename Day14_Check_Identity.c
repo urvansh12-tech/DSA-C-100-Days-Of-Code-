543d6d94967e6ca36e0c3cda28b1a8cd9c8bfa3c
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2026-02-23 22:43:29 +0530

    Update and rename DAY14_CHECK_IDENTITY_MATRIX.c to Day14_Check_Identity.c

--- Day14_Check_Identity.c
+++ Day14_Check_Identity.c
@@ -1,22 +1,4 @@
-/*Problem: Write a program to check whether a given square matrix is an Identity Matrix. An identity matrix is a square matrix in which all diagonal elements are 1 and all non-diagonal elements are 0.
-
-Input:
-- First line: integer n representing number of rows and columns
-- Next n lines: n integers each representing the matrix elements
-
-Output:
-- Print "Identity Matrix" if the matrix satisfies the condition
-- Otherwise, print "Not an Identity Matrix"
-
-Example:
-Input:
-3
-1 0 0
-0 1 0
-0 0 1
-
-Output:
-Identity Matrix*/
+/*Problem: Write a program to check whether a given square matrix is an Identity Matrix. An identity matrix is a square matrix in which all diagonal elements are 1 and all non-diagonal elements are 0*/
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
@@ -60,4 +42,5 @@ int main() {
     }
     free(matrix);
     return 0;
+
 }
