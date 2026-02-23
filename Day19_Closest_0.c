11ab2908880c8a9f155362a18fafcb9c511a47c7
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2026-02-23 22:47:46 +0530

    Update Day19_Closest_0.c

--- Day19_Closest_0.c
+++ Day19_Closest_0.c
@@ -1,21 +1,4 @@
-/*Problem: Given an array of integers, find two elements whose sum is closest to zero.
-
-Input:
-- First line: integer n
-- Second line: n space-separated integers
-
-Output:
-- Print the pair of elements whose sum is closest to zero
-
-Example:
-Input:
-5
-1 60 -10 70 -80
-
-Output:
--10 1
-
-Explanation: Among all possible pairs, the sum of -10 and 1 is -9, which is the closest to zero compared to other pairs.*/
+/*Problem: Given an array of integers, find two elements whose sum is closest to zero*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -64,3 +47,4 @@ int main() {
     free(arr);
     return 0;
 }
+
