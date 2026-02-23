896ea0a50caf941308df3e160db463e4d3940f68
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2026-02-23 22:34:12 +0530

    Update Day7_Fibbonacci.c

--- Day7_Fibbonacci.c
+++ Day7_Fibbonacci.c
@@ -1,19 +1,4 @@
-/*Problem: Write a recursive function fib(n) to compute the n-th Fibonacci number where fib(0)=0 and fib(1)=1.
-
-Input:
-- Single integer n
-
-Output:
-- Print the n-th Fibonacci number
-
-Example:
-Input:
-6
-
-Output:
-8
-
-Explanation: Sequence: 0,1,1,2,3,5,8 at positions 0,1,2,3,4,5,6*/
+/*Problem: Write a recursive function fib(n) to compute the n-th Fibonacci number where fib(0)=0 and fib(1)=1*/
 #include <stdio.h>
 
 int fib(int n) {
@@ -31,3 +16,4 @@ int main() {
     printf("%d", fib(n));
     return 0;
 }
+
