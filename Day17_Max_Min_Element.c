19c4e66591d36cf436e543f10588f90604a6af0c
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2026-02-23 22:46:10 +0530

    Update and rename DAY17_MAX_MIN_ELEMENT.c to Day17_Max_Min_Element.c

--- Day17_Max_Min_Element.c
+++ Day17_Max_Min_Element.c
@@ -1,20 +1,4 @@
-/*Problem: Write a program to find the maximum and minimum values present in a given array of integers.
-
-Input:
-- First line: integer n
-- Second line: n integers
-
-Output:
-- Print the maximum and minimum elements
-
-Example:
-Input:
-6
-3 5 1 9 2 8
-
-Output:
-Max: 9
-Min: 1*/
+/*Problem: Write a program to find the maximum and minimum values present in a given array of integers*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -50,4 +34,5 @@ int main() {
     free(arr);
 
     return 0;
+
 }
