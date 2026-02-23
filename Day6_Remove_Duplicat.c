2618dd2cc59a5577c3cde7588a93887f12415848
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2026-02-23 22:33:51 +0530

    Update Day6_Remove_Duplicat.c

--- Day6_Remove_Duplicat.c
+++ Day6_Remove_Duplicat.c
@@ -1,21 +1,4 @@
-/*Problem: Given a sorted array of n integers, remove duplicates in-place. Print only unique elements in order.
-
-Input:
-- First line: integer n
-- Second line: n space-separated integers (sorted array)
-
-Output:
-- Print unique elements only, space-separated
-
-Example:
-Input:
-6
-1 1 2 2 3 3
-
-Output:
-1 2 3
-
-Explanation: Keep first occurrence of each element: 1, 2, 3*/
+/*Problem: Given a sorted array of n integers, remove duplicates in-place. Print only unique elements in order.*/
 #include <stdio.h>
 int main() {
     int n;
@@ -34,3 +17,4 @@ int main() {
     }   
     return 0;
 }
+
