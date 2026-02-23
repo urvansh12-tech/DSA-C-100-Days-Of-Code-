e79281e91665a2131211ab70c927431b4eb73c90
URVANSH AGARWAL <227296010+urvansh12-tech@users.noreply.github.com>
2026-02-23 22:46:57 +0530

    Update and rename DAY18_ROTATE_ARRAY_BY_K.c to Day18_Rotate_Array.c

--- Day18_Rotate_Array.c
+++ Day18_Rotate_Array.c
@@ -1,21 +1,4 @@
-/*Problem: Given an array of integers, rotate the array to the right by k positions.
-
-Input:
-- First line: integer n
-- Second line: n integers
-- Third line: integer k
-
-Output:
-- Print the rotated array
-
-Example:
-Input:
-5
-1 2 3 4 5
-2
-
-Output:
-4 5 1 2 3*/
+/*Problem: Given an array of integers, rotate the array to the right by k positions*/
 #include <stdio.h>
 int main() {
     int n, k;
@@ -35,3 +18,4 @@ int main() {
 
     return 0;
 }
+
